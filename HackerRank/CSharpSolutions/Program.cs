61696b7290b00b5962adc3479baa35028ff5a783
Radoslaw <ko.rad@o2.pl>
2019-06-25 15:20:40 +0200

    Dynamic Programming - MaxArraySum

---
+++ HackerRank/CSharpSolutions/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { -2, 1, 3, -4, 5 };
+            int trol = DynamicProgramming.DynamicProgramming.MaxArraySum(arr);
+
+            int qq = 0;
+        }
+    }
+}
